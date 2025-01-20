65dd0ddf3278640b632746561438f1957c0dd6c6
Dixit <dixitbhojani0@gmail.com>
2025-01-21 01:54:07 +0530

    Add basic pipeline changes

---
+++ Jenkinsfile
@@ -0,0 +1,30 @@
+pipeline {
+
+    agent any
+
+
+    stages {
+
+        stage('Install Dependencies') {
+
+            steps {
+
+                bat 'npm install'
+
+            }
+
+        }
+
+        stage('Run Tests') {
+
+            steps {
+
+                bat 'npm test'
+
+            }
+
+        }
+
+    }
+
+}
---
+++ app.test.js
@@ -0,0 +1,13 @@
+// app.test.js
+
+const request = require('supertest');
+
+const app = require('./app');
+
+describe('GET /', () => {
+    it('should return Hello world', async() => {
+        const res = await request(app).get('/');
+        expect(res.statusCode).toBe(200);
+        expect(res.text).toBe('Hello world');
+    });
+});
