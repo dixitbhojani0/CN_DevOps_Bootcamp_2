427a7ec353e9a537c96daecc8476d8ad22b7cfe5
dixitbhojani0 <60063369+dixitbhojani0@users.noreply.github.com>
2025-01-26 01:28:55 +0530

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -5,9 +5,9 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('GET /', () => {
-    it('should return Hello world', async() => {
+    it('should return Hello world from the other side 2', async() => {
         const res = await request(app).get('/');
         expect(res.statusCode).toBe(200);
-        expect(res.text).toBe('Hello world');
+        expect(res.text).toBe('Hello world from the other side 2');
     });
 });
